060fb3d24f43fc9ff658820f3b6ee09741e2a947
Lawrence Woodman <lwoodman@vlifesystems.com>
2013-02-20 15:24:31 +0000

    Refactor into procs

--- bin2c.tcl
+++ bin2c.tcl
@@ -9,32 +9,47 @@ if {$argc != 1} {
   puts "Usage: bin2c <inputFilename>"
   exit
 }
-set filename [lindex $argv 0]
 
-set fid [open $filename r]
-chan configure $fid -translation binary
-set data [read $fid]
-close $fid
-
-binary scan $data H* hex
-set hex [regsub -all {(..)} $hex {\1 }]
-set hex [string trim $hex]
-set hex [split $hex]
-set dataLength [llength $hex]
-
-set output "unsigned char $filename\[\] = {\n"
-for {set i 0} {$i < $dataLength} {incr i} {
-  set num [lindex $hex $i]
-  append output " 0x$num"
-  if {$i != $dataLength-1} {
-    append output ","
-    if {($i+1) % 12 == 0} {
-      append output "\n"
-    }
-  }
+proc readBinaryFile {filename} {
+  set fid [open $filename r]
+  chan configure $fid -translation binary
+  set contents [read $fid]
+  close $fid
+  return $contents
 }
 
-append output "\n};\n"
-append output "unsigned int " $filename "_len = $dataLength;"
+proc string2Hex {string} {
+  binary scan $string H* hex
+  set hex [regsub -all {(..)} $hex {\1 }]
+  set hex [string trim $hex]
+  return [split $hex]
+}
 
-puts $output
+
+proc outputC {hexList arrayName outFid} {
+  set hexLength [llength $hexList]
+  puts $outFid "unsigned char $arrayName\[\] = {"
+  for {set i 0} {$i < $hexLength} {incr i} {
+    if {$i % 12 == 0} {puts -nonewline " "}
+
+    set num [lindex $hexList $i]
+    puts -nonewline $outFid " 0x$num"
+
+    if {$i != $hexLength-1} {
+      puts -nonewline $outFid ","
+      if {($i+1) % 12 == 0} {
+        puts $outFid ""
+      }
+    }
+  }
+  puts $outFid "\n};"
+  puts -nonewline $outFid "unsigned int $arrayName"
+  puts $outFid "_len = $hexLength;"
+}
+
+set filename [lindex $argv 0]
+set inputFileContents [readBinaryFile $filename]
+set hex [string2Hex $inputFileContents]
+set outFid stdout
+
+outputC $hex $filename $outFid
